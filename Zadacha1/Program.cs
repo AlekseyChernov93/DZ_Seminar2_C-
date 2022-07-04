367b60063c9f7be581e47d83a15ed2d9dea096d5
Aleksey Chernov <n9613522959@yandex.ru>
2022-07-04 16:44:39 +0300

    Сохранение

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает
+на вход трёхзначное число и
+на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int Znach3()
+{
+    Console.WriteLine("Введите трехзначное число ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int des = n % 100;
+    int n2 = des / 10;
+    return n2;
+}
+int result = Znach3();
+Console.WriteLine($"Третье число введеного числа:  {result}");
